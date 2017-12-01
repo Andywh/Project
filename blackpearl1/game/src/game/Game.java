c5ac139823401098cb364e72e68ee1f48fc13218
Andywh <1363703905@qq.com>
2017-12-01 14:21:25 +0800

    blackpearl1

---
+++ blackpearl1/game/src/game/Game.java
@@ -0,0 +1,8 @@
+package game;
+import view.*;
+
+public class Game {
+    public static void main(String[] args) {
+        new LoginFrame().launchFrame();
+    }
+}
---
+++ blackpearl1/game/src/model/UserInfo.java
@@ -0,0 +1,40 @@
+package model;
+
+public class UserInfo {
+	private String userName;
+	private String passWord;
+	private int level;
+	private int money;
+	
+	public UserInfo(String userName, String passWord) {
+		this.userName = userName;
+		this.passWord = passWord;
+		this.level = 1;
+		this.money = 0;
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	public String getPassWord() {
+		return this.passWord;
+	}
+
+	public int getLevel() {
+		return this.level;
+	}
+
+	public int getMoney() {
+		return this.money;
+	}
+	
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	
+}
